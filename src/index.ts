ebc11d116d0d64ac16f769c0875ac6339c406585
Luke Zigler <luke.zigler98@gamil.com>
2024-12-02 18:53:26 +0000

    Shuffled around folder structure

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-import { createServer } from "./server";
+import { createServer } from "./server/server";
 import { FastifyInstance } from "fastify";
 
-createServer().then((server: FastifyInstance) => {
-  server.log.info(`server started on port 3000`);
-}).catch(err => {
-  console.error(`Could not start server: ${err}`);
-  process.exit(1)
-})
+createServer()
+  .then((server: FastifyInstance) => {
+    server.log.info(`server started on port 3000`);
+  })
+  .catch((err) => {
+    console.error(`Could not start server: ${err}`);
+    process.exit(1);
+  });

33cc48e94e2d9b917366547f9f82b8df9c608145
Luke Zigler <luke.zigler98@gamil.com>
2024-12-27 14:37:18 +0000

    Passed up fact edit routes init to fact routes init

--- src/server/routes/admin/facts/:id/index.ts
+++ src/server/routes/admin/facts/:id/index.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from "fastify";
 import { factGetRoute } from "./get";
+import { initFactEditRoutes } from "./edit";
 
 export function initFactRoutes(server: FastifyInstance) {
+  initFactEditRoutes(server);
   server.route(factGetRoute);
 }

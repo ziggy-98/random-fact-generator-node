d6700d46ea8be068421834a8b286036639892ab4
Luke Zigler <luke.zigler98@gamil.com>
2025-01-13 09:07:00 +0000

    Made sure that gradient background is only applied to home pages

--- src/server/utils/handlebars/helpers/inlineIf.ts
+++ src/server/utils/handlebars/helpers/inlineIf.ts
@@ -1,3 +1,12 @@
-export function inlineIf(input: string, value: string) {
+export function inlineIf(input: string, evaluation: boolean, value: string, not?: boolean) {
+  if ((evaluation && input && !not) || (evaluation && !input && not)) {
+    return value;
+  }
+  if ((evaluation && !input) || (evaluation && input && not)) {
+    return;
+  }
+  if (!evaluation && not) {
+    return input !== value;
+  }
   return input === value;
 }
